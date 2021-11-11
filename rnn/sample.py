498b56c37303c1ad016524af385f5beeac01ee0d
Seven <seven.zhan92@gmail.com>
2021-11-11 10:32:30 +0800

    minor update

--- rnn/sample.py
+++ rnn/sample.py
@@ -2,11 +2,16 @@ import caffe
 caffe.set_mode_cpu()
 import numpy as np
 
+input_size = 10
+hidden_size = 15
+batch_size = 1
+seq_len = 3
+num_layers = 1
 
 # inputs
-input_x = np.random.randn(3, 1, 10)
-input_cont = np.random.randn(3, 1)
-input_h = np.random.randn(1, 1, 15)
+input_x = np.random.randn(seq_len, batch_size, input_size)
+input_cont = np.random.randn(seq_len, batch_size)
+input_h = np.random.randn(num_layers, batch_size, hidden_size)
 
 # model
 model_caffe = caffe.SGDSolver('solver.prototxt')
@@ -19,8 +24,8 @@ model_caffe.net.forward()
 # outputs
 caffe_output_y = model_caffe.net.blobs['output_y'].data[...]
 caffe_output_h = model_caffe.net.blobs['output_h'].data[...]
-print(caffe_output_h.shape)
 print(caffe_output_y.shape)
+print(caffe_output_h.shape)
 
 # parameters: weights and bias
 params = model_caffe.net.params['rnn1']
@@ -32,24 +37,24 @@ w_xh, b_xh = params[0].data[...], params[1].data[...]
 w_hh = params[2].data[...]
 w_ho, b_ho = params[3].data[...], params[4].data[...]
 
-# w_xh_x = fc(w_xh*x + b_xh)
-# h_cont = h * cont
-# w_hh_h = fc(w_hh*h_cont)
-# h = tanh(w_xh_x + w_hh_h)
-# y = tanh(fc(w_ho*h + b_ho))
+# ^ w_xh_x = fc(w_xh*x + b_xh)
+# ^ h_cont = h * cont
+# ^ w_hh_h = fc(w_hh*h_cont)
+# ^ h = tanh(w_xh_x + w_hh_h)
+# ^ y = tanh(fc(w_ho*h + b_ho))
 numpy_output_h = input_h
-numpy_output_ys = []
+numpy_output_ys = np.zeros((seq_len, batch_size, hidden_size))
+w_xh_x = np.dot(input_x, w_xh.T) + b_xh
 for i in range(input_x.shape[0]):
-    h_cont = numpy_output_h * input_cont[i]
-    w_xh_x = np.dot(input_x[i], w_xh.T) + b_xh
+    h_cont = numpy_output_h * input_cont[i][0, ...]
     w_hh_h = np.dot(h_cont, w_hh.T)
-    numpy_output_h = np.tanh(w_xh_x + w_hh_h)
-    numpy_output_y = np.tanh(np.dot(numpy_output_h, w_ho.T) + b_ho)
-    numpy_output_ys.append(numpy_output_y)
-numpy_output_y = np.concatenate(numpy_output_ys, axis=0)
+    numpy_output_h = np.tanh(w_xh_x[i] + w_hh_h)
+    numpy_output_ys[i] = numpy_output_h
+numpy_output_y = np.tanh(np.dot(numpy_output_ys, w_ho.T) + b_ho)
 
 # check the diff
 diff_h = np.abs(caffe_output_h - numpy_output_h)
 diff_o = np.abs(caffe_output_y - numpy_output_y)
-print(diff_h.shape, diff_h.min(), diff_h.max())
-print(diff_o.shape, diff_o.min(), diff_o.max())
+print()
+print('diff between h:', diff_h.min(), diff_h.max())
+print('diff between y:', diff_o.min(), diff_o.max())
